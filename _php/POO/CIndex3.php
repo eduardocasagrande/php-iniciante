20572ae525d66c05897d1aa6caeb727d78e302c2
Eduardo Casagrande <eduardo.casagrande@engeplus.com.br>
2018-02-27 18:04:13 -0300

    Teste

---
+++ _php/POO/CIndex3.php
@@ -0,0 +1,6 @@
+<?php
+class Carro{
+    public $marca;
+    public $cor;
+    public $motor;
+}
---
+++ _php/POO/index3.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once("CIndex3.php");
+
+$fusion = new Carro();
+$civic = new Carro();
+
+echo $fusion->marca = "Ford";
+$fusion->cor = "Preto";
